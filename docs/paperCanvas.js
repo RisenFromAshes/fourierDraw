d037b6b6e5040e1b6dd64e1ac0ae9f7340f5b47f
RisenFromAshes <ashrafurkhan37@gmail.com>
2019-05-07 19:05:25 +0600

    stroke update

--- docs/paperCanvas.js
+++ docs/paperCanvas.js
@@ -5,22 +5,27 @@ function onMouseDown(event) {
     myPath.strokeWidth = strokeWidth;
     myPath.fillColor = fillColor
     myPath.strokeCap = 'round';
-    myPath.add(event.point);
+    myPath.strokeJoin = 'round';
     myPath.selected = false;
+    myPath.add(event.point);
 }
 
 function onMouseDrag(event) {
-    myPath.add(event.point)
+    myPath.add(event.point);
 }
 
 function onMouseUp(event) {
-    myPath.simplify(10)
+    myPath.simplify(10 * strokeWidth / 4);
+    //myPath.flatten(0);
     myPath.selected = true;
 }
 
 globals.setFillColor = function (color) {
-    if (myPath) myPath.fillColor = fillColor
+    if (myPath) myPath.fillColor = fillColor;
 }
 globals.setStrokeColor = function (color) {
-    if (myPath) myPath.strokeColor = strokeColor
+    if (myPath) myPath.strokeColor = strokeColor;
+}
+globals.setStrokeWidth = function (width) {
+    if (myPath) myPath.strokeWidth = width;
 }
